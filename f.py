1c8ebe9f2be88e5a453f61e0bbbb2d6639854197
ahmed achour <66746240+17med@users.noreply.github.com>
2023-04-06 14:20:49 +0100

    Update f.py

--- f.py
+++ f.py
@@ -1,46 +1,51 @@
 import websocket
 import _thread
 import time
-import rel
 from os import system
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 from datetime import datetime
 from time import sleep
+
 def date():
     return (datetime.now()).strftime("%m/%d/%Y %H:%M:%S")
-def savedata(id,x):
+
+def savedata(id, x):
     global db
    
     data = x
 
     doc_ref = db.collection('MATIC').document(str(id))
     doc_ref.set(x)
-    print("id {} x {}".format(id,x))
+    print("id {} x {}".format(id, x))
+
 def on_message(ws, message):
     sleep(30)
-    x=eval(message)
-    remaining=x['events'][0]["remaining"]
-    eventId=x['eventId']
-    types=x['events'][0]['type']
+    x = eval(message)
+    remaining = x['events'][0]["remaining"]
+    eventId = x['eventId']
+    types = x['events'][0]['type']
     
-    savedata(eventId,{
-        "date":date(),
-        "price":x['events'][0]['price'],
-        "remaining":remaining
+    savedata(eventId, {
+        "date": date(),
+        "price": x['events'][0]['price'],
+        "remaining": remaining
     })
+    
     print(f"id :{eventId} price :{x['events'][0]['price']} remaining : {remaining}")
     exit
-    pass
+
 def on_error(ws, error):
     #print(error)
     pass
+
 def on_close(ws, close_status_code, close_msg):
     print("### closed ###")
 
 def on_open(ws):
     print("Opened connection")
+
 def lt():
     websocket.enableTrace(False)
     ws = websocket.WebSocketApp("wss://api.gemini.com/v1/marketdata/MATICUSD",
@@ -48,14 +53,14 @@ def lt():
                               on_message=on_message,
                               on_error=on_error,
                               on_close=on_close)
+    
     system("clear")
     print("hi")
-    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
-    rel.signal(2, rel.abort)  # Keyboard Interrupt
-    rel.dispatch()
+    # Set the ping interval to 5 seconds and the ping timeout to 2 seconds
+    ws.run_forever(ping_interval=5, ping_timeout=2)  
+    
 
 if __name__ == "__main__":
-    
     # Initialize Firebase with your credentials
     cred = credentials.Certificate('m.json')
     firebase_admin.initialize_app(cred)
@@ -64,4 +69,3 @@ if __name__ == "__main__":
     db = firestore.client()
     lt()
     print("hi")
-    
